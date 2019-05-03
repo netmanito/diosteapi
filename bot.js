e75ee19a0d94724b02695823d78c83922b1ca970
jacinto calvo <jaci@seriousman.org>
2019-05-03 20:22:01 +0200

    added telegram bot file

---
+++ bot.js
@@ -0,0 +1,74 @@
+var client = require('./connection.js');
+var argv = require('yargs').argv;
+var functions = require('./functions.js');
+const TelegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = 'YOUR_TELEGRAM_BOT_TOKEN';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+// Matches "/echo [whatever]"
+bot.onText(/^\/quote/, function(msg){
+  console.log(msg);
+
+  const chatId = msg.chat.id;
+  const username = msg.from.username;
+    functions.quote(msg.quote), function(quote){
+      bot.sendMessage(chatId, quote);
+    }
+});
+
+/* if (argv.search) {
+  var quoteLookup=argv.search;
+  console.log("Search term: "+quoteLookup);
+  results(quoteLookup);
+} */
+
+// function search word in quote
+
+if (argv.quote) {
+  functions.quote(argv.quote, function(quote) {
+      console.log(results);
+  });
+} 
+
+// function search quote by author
+if (argv.aquote) {
+  functions.aquote(argv.aquote, function(aquote) {
+    console.log(aquote);
+ });
+}
+
+// function search quote by id
+if (argv.iquote) {
+  functions.iquote(argv.iquote, function(iquote) {
+    console.log(iquote);
+ });
+}
+
+// function random quote
+if (argv.rquote) {
+  functions.rquote(function(rquote) {
+     console.log(rquote);
+  });
+}
+
+// function addquote 
+if (argv.addquote) {
+  functions.addquote(argv.addquote, function(addquote) {
+    console.log(addquote);
+ });
+}
+
+// function delquote 
+if (argv.delquote) {
+  functions.delquote(argv.delquote, function(delquote) {
+    console.log(delquote);
+ });
+}
+
+// ADD THESE TWO LINES
+//app.use('/rquote', results);
+//module.exports = app;
